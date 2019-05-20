61c96cc54caf1655b72a040fe2c1ae585c7bea0d
Dan-Vy Le <danvyle85@gmail.com>
2019-05-20 23:49:26 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -14,16 +14,3 @@ end
 
 
 # [1,2,3,4,5].select { |num|  num.even?  }   #=> [2, 4]
-
-# def my_select(array) 
-# count = 0
-# result = []
-
-# while count < array.size
-#   if yield(array[count]) == true
-#   result.push(array[count])
-# end
-#   count+=1
-# end
-# result
-# end
